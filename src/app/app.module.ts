5add51ed3a0f9879bbdff8621c97961c2db1bfba
adrimartin19 <adrimartin19@github.com>
2020-10-19 21:49:38 +0200

    Subida Proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/nav/nav.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+import {  animate,
+  query,
+  stagger,
+  style,
+  transition,
+  trigger 
+} from '@angular/animations';
+
+const listAnimation = trigger('listAnimation', [
+  transition('* <=> *', [
+    query(':enter',
+      [style({ opacity: 0 }), stagger('60ms', animate('600ms ease-out', style({ opacity: 1 })))],
+      { optional: true }
+    ),
+    query(':leave',
+      animate('200ms', style({ opacity: 0 })),
+      { optional: true}
+    )
+  ])
+]);
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss'],
+  animations: [listAnimation]
+})
+export class NavComponent{
+
+  titulo: string = ""
+  elementos: Array<string> = []
+  seleccionado: boolean = false
+  elementoSel: string = ""
+  backTo: string = "Back To Index"
+
+  constructor() { 
+    this.titulo = 'Adrizzy on Film'
+  }
+
+  navList(){
+    this.elementos = this.elementos.length ? [] : ['Carrete 1', 'Verano', 'Carrete 2'];
+  }
+
+
+  setSeleccionado(sel){
+    this.elementoSel = sel;
+    this.backTo = sel;
+    this.seleccionado = true;
+  }
+
+  isSeleccionado(){
+    return this.seleccionado;
+  }
+
+}
+
