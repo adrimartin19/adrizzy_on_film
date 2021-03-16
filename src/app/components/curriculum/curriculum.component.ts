19026efee85cae9b3cd2cd00e74b8ed1b57f4d12
adrimartin19 <adrimartin19@github.com>
2021-03-16 16:38:11 +0100

    home final update

--- src/app/components/curriculum/curriculum.component.ts
+++ src/app/components/curriculum/curriculum.component.ts
@@ -1,9 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import {  animate,
+  style,
+  transition,
+  trigger 
+} from '@angular/animations';
+
+
+export const fadeAnimation = trigger('fadeAnimation', [
+  transition(':enter', [
+    style({ opacity: 0 }), animate('2000ms 500ms', style({ opacity: 1 }))]
+  ),
+  transition(':leave',
+    [style({ opacity: 1 }), animate('300ms', style({ opacity: 0 }))]
+  )
+]);
 
 @Component({
   selector: 'app-curriculum',
   templateUrl: './curriculum.component.html',
-  styleUrls: ['./curriculum.component.scss']
+  styleUrls: ['./curriculum.component.scss'],
+  animations: [fadeAnimation]
 })
 export class CurriculumComponent implements OnInit {
 
--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,57 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-import { Album } from 'src/app/interfaces/album';
-import {  animate,
-  query,
-  stagger,
-  style,
-  transition,
-  trigger 
-} from '@angular/animations';
 import { Router } from '@angular/router';
-import { Image } from 'src/app/interfaces/image';
 
-const listAnimation = trigger('listAnimation', [
-  transition('* <=> *', [
-    query(':enter',
-      [style({ opacity: 0 }), stagger('60ms', animate('600ms ease-out', style({ opacity: 1 })))],
-      { optional: true }
-    )
-  ])
-]);
-
-export const fadeAnimation = trigger('fadeAnimation', [
-  transition(':enter', [
-    style({ opacity: 0 }), animate('500ms', style({ opacity: 1 }))]
-  )
-]);
-
-export const fadeSlider = trigger('fadeSlider', [
-  transition(':enter', [
-    style({ opacity: 0 }), animate('300ms', style({ opacity: 1 }))]
-  ),
-  transition(':leave', [
-    style({ opacity: 1 }), animate('300ms', style({ opacity: 0 }))
-  ])
-]);
 
 
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss'],
-  animations: [listAnimation, fadeAnimation, fadeSlider]
+  styleUrls: ['./nav.component.scss']
 })
 
 export class NavComponent implements OnInit{
 
   titulo: string = ""
-  albums: Array<Album> = []
-  showInfo: boolean = false
-  showList: boolean = false
-  hiddenNav: string = ">>"
-  isHiddenNav: boolean = true
-  imagenes: Array<Image> = []
+  aboutme: string = ""
+  back: string = ""
 
   ngOnInit(){
   }
@@ -61,49 +24,8 @@ export class NavComponent implements OnInit{
   ) 
   { 
     this.titulo = 'Adrizzy on Film'
-    
-  }
-
-  navList(){
-    this.albums = this.albums.length ? [] : 
-    [{Name: "Carrete 1", LinkRoute: "carrete1"}, 
-    {Name: "Un finde de Cumpleaños", LinkRoute: "BirthdayWeekend"}, {Name: "Carrete 2", LinkRoute: "carrete2"}
-    ];
-    
-    if(!this.showList ) document.getElementById("selectPhoto").style.textDecoration = "underline solid 5px"
-    else document.getElementById("selectPhoto").style.textDecoration = ""
-    this.showList = !this.showList
-  }
-
-  showSlider(){
-      this.navList()
-  }
-
-  info(){
-    if(!this.showInfo) document.getElementById("selectInfo").style.textDecoration = "underline solid 5px"
-    else document.getElementById("selectInfo").style.textDecoration = ""
-    this.showInfo = !this.showInfo 
-  }
-
-  isShowInfo(){
-    return this.showInfo
-  }
-
-  isShowList(){
-    return this.showList
-  }
-
-  getHiddenNav(){
-    return !this.isHiddenNav;
-  }
-
-  toHiddenNav(): void{
-      if(this.isHiddenNav)
-        this.hiddenNav = "<<"
-      else
-        this.hiddenNav = ">>"
-      
-      this.isHiddenNav = !this.isHiddenNav
+    this.aboutme = 'Sobre mí'
+    this.back = 'Atrás'
   }
 
 }
